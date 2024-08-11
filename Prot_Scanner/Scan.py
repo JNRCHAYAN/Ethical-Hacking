ddc51e2c8305b31d353f337f36bafc80c339907f
jnrchayan <jnrchayan@gmail.com>
2024-08-11 15:23:17 +0600

    Port Scanner project

---
+++ Prot_Scanner/Scan.py
@@ -0,0 +1,15 @@
+import socket
+from IPy import IP
+
+
+ipadress = input("[+] Enter Target IP to Scan : ")
+port = 80
+try:
+    sock = socket.socket()
+    sock.connect((ipadress),port)
+    print(f"[+] Port {port} is Open")
+    
+except:
+    print(f"[-] Port {port} is close")
+
+
---
+++ Prot_Scanner/readme.txt
@@ -0,0 +1,6 @@
+1. Add IPy - pip install IPy
+2. Find Website IP Address using nslockup  command 
+
+                Install nslockup in kali lenux 
+                -> sudo apt install dnsutils 
+3. 
